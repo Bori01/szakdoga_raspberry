264c9a7d93be09e7e074e7c8e14aaee7ff536b9c
Bori01 <bori.nagy01@gmail.com>
2023-11-20 21:58:17 +0100

    better

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import busio
 import adafruit_tsl2591
 import adafruit_si7021
 import Adafruit_ADS1x15
-from gpiozero import LED
 import RPi.GPIO as GPIO
 import mqtt_connect
 import publisher
@@ -14,7 +13,7 @@ i2c = board.I2C()
 
 lightsensor = adafruit_tsl2591.TSL2591(i2c)
 thsensor = adafruit_si7021.SI7021(board.I2C())
-adc = Adafruit_ADS1x15.ADS1015(address=0x49, busnum=1)
+adc = Adafruit_ADS1x15.ADS1015(address=0x48, busnum=1)
 GAIN = 1
 
 relay = 18
@@ -28,26 +27,26 @@ temperature_topic = "sector2/temperature"
 humidity_topic = "sector2/humidity"
 soilmoisture_topic = "sector2/soilmoisture"
 
-KEVES_VIZ = False
+KEVES_VIZ = True
 
-def on_watering(client, userdata, msg):
+def on_message(client, userdata, msg):
     print(f"Recieved `{msg.payload.decode()}` from `{msg.topic}` topic")
-    if not KEVES_VIZ:
-        print("locsol")
-        GPIO.output(relay, True)
-        time.sleep(5.0)
-        GPIO.output(relay, False)
-        
-def on_waterlevel_change(client, userdata, msg):
-    print(f"Recieved `{msg.payload.decode()}` from `{msg.topic}` topic")
-    if (msg.topic).find("waterlevel"):
-        if (msg.payload.decode()).find("danger"):
+    if (msg.topic).find("sector2/water") != -1:
+        if not KEVES_VIZ:
+            print("locsol")
+            GPIO.output(relay, True)
+            time.sleep(5.0)
+            GPIO.output(relay, False)
+
+    if (msg.topic).find("general/waterlevel") != -1:
+        if (msg.payload.decode()).find("danger") != -1:
             KEVES_VIZ = True
-        elif (msg.payload.decode()).find("enough"):
+        elif (msg.payload.decode()).find("enough") != -1:
             KEVES_VIZ = False
+    
             
-subscriber.subscribe(client, "general/waterlevel", on_waterlevel_change)
-subscriber.subscribe(client, "sector2/water", on_watering)
+subscriber.subscribe(client, "general/waterlevel", on_message)
+subscriber.subscribe(client, "sector2/water", on_message)
 subscriber.run(client)
 
 while True:
